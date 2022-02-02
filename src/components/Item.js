70fb1e65f0d0838bc5c3d097bdeb4a2d5e29c4fb
Olha Gruzglina <olga.zhmura@gmail.com>
2022-02-02 13:34:50 -0500

    commit tree c37703f009f5b2bb357601b8c5c43f4dee53a4a2

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [ isAddedToCart, setAddedToCart ] = useState(false);
+  const liClassName = isAddedToCart ? "in-cart" : "";
+  const btnClassName = isAddedToCart ? "remove" : "add";
+
+  function handleClickCartBtn () {
+    setAddedToCart (isAddedToCart => !isAddedToCart);
+  }
+
   return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+    <li className={ liClassName }>
+      <span>{ name }</span>
+      <span className="category">{ category }</span>
+      <button className={ btnClassName } onClick = { handleClickCartBtn }>
+        { isAddedToCart ? 'Remove From Cart' : 'Add to Cart' }
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,21 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
+// Use the useState hook to create a state variable called selectedCategory for keeping 
+//track of the selected value from this <select> element. When the value of the <select> 
+//element is changed, update state.
+
+// You should also use the selectedCategory variable to determine which items to display 
+//in the shopping list. You'll need some way of filtering the array of items based on the 
+//selectedCategory.
+
+
 function ShoppingList({ items }) {
+  const options = [
+    { 
+      value: 'All', 
+      textContent: "Filter by category", 
+    },
+    {
+      value: "Produce",
+      textContent: 'Produce'
+    },
+    { 
+      value: "Dairy",
+      textContent: 'Dairy',
+    },
+    {
+      value: "Dessert", 
+      textContent: 'Dessert',
+    }
+  ];
+
+  const optionsContent = options.map( ({ value, textContent }) =>
+    <option key = { value } value = { value }>{ textContent }</option>
+  );
+
+  const [ selectedCategory, setSelectedCategory ] = useState("All");
+
+  const arrOfSelectedItems = (selectedCategory === 'All') ? 
+    [...items] :
+    items.filter(item => item.category === selectedCategory);
+
+  const addItem = arrOfSelectedItems.map((item) => (
+    <Item key = { item.id } { ...item } />
+  ));
+
+
+  function handleSelectionChange (e) {
+    const selectedValue = e.target.value;
+    console.log(selectedValue);
+
+    setSelectedCategory(selectedCategory => selectedValue);
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
+        <select name="filter" onChange={handleSelectionChange}>
+          {optionsContent}
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {addItem}
       </ul>
     </div>
   );
